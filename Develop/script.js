381644085300a6c364345e5bed1634528fb42e5f
Albedo <raincadence27@gmail.com>
2022-02-26 22:22:38 -0800

    hour int

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,38 @@
 //come back later to add 24 hour update function
 
 const today = moment().format("MMM Do, YYYY")
+
 document.getElementById('currentDay').innerHTML ='Today is ' + today
 
 //console.log(currentDate)
 
 //section - timeblocks
 
-const 
+let hourSlot = document.getElementsByClassName('hour')
+let timeRow = document.getElementsByClassName('row')
 
-let timeValue = document.getElementsByClassName('hour')
+let hour = parseInt(moment().format('h'))
 
-if (condition) {
+
+
+// for loop attempt at row creation
+
+// for (let i = 0; i < 8; i++) {
+//   let midday
+//   if (i < 3) {
+//     midday = 'AM'
+//   }
+//   else {
+//     midday = 'PM'
+//   }
+//   const rowDiv = timeRow.createElement('<div>')
+//   const rowText = timeRow.createElement('textarea')
+//   const rowBtn = timeRow.createElement('button')
+
+//   rowDiv[i].classList.add('hour')
+//   rowDiv[i].innerHTML = ([i+9]+midday)
+//   document.getElementsByClassName('hour').appendChild(rowDiv)
+//   rowText[i].classList.add('description')
+//   rowBtn[i].classList.add('saveBtn')
   
-}
+// }
