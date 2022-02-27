764351dcc486ff9d220179f425f7c1617d2f07a5
Albedo <raincadence27@gmail.com>
2022-02-26 16:44:05 -0800

    today's date

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+//section - current day displayed at top
+//come back later to add 24 hour update function
+
+const today = moment().format("MMM Do, YYYY")
+document.getElementById('currentDay').innerHTML ='Today is ' + today
+
+//console.log(currentDate)
+
+//section - timeblocks
+
